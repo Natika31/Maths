2ab735e88f824d8132321edd5208ebdf231c2b61
Trazard <34913527+Trazard@users.noreply.github.com>
2018-06-10 13:52:04 +0200

    suppression du mauvais faddev

--- modele/fadeev.py
+++ modele/fadeev.py
@@ -1,49 +1,86 @@
 import numpy as np
+import sys
 
 
 def generateurDeMatriceAleatoire(dimension=3,min=0,max=1):
 	return np.random.randn(dimension,dimension) * (max - min +1) + min
 
-def methodeTraces(A1,A=None,B=None,n=1):
-	#verifier que le det != 0 + expliquer la fonction det si elle existe sinon la faire
-	print("[",n,"]")
+def matInverse(A1,A=None,B=None,n=1):
 	if np.linalg.det(A1) != 0.:
 		if n==1:
-			q = np.trace(A1)	#expliquer trace
-			B = A1 - np.dot(q,np.identity(len(A1))) #expliquer dot et identity
+			q = np.trace(A1)	
+			B = A1 - np.dot(q,np.identity(len(A1)))
 		else:
 			A = np.dot(A1,B)
 			q = np.trace(A)/n
 			
 			Bold = B
 			B = A - np.dot(q,np.identity(len(A)))
-
-		print("Qn : \n",q)
-		if n > 1:	
-			print("An : \n",A)
-			print("Bn-1 : \n",B)
-		print("Bn : \n",B)
-
+		
 		if B.all() == 0:
-
-			return np.divide(Bold,q)
+			return str(np.divide(Bold,q)),q
 		else:	
-			return methodeTraces(A1,A,B,n+1)
+			return matInverse(A1,A,B,n+1)
 	else:
-		print("Déterminant == 0")
-		return None
+		print("e0")
+	return None
 
 
 
-mat = generateurDeMatriceAleatoire(dimension = 50,min=0,max=5)
-print("Matrice : \n",mat)
-print("Déterminant : ",np.linalg.det(mat))
-i = np.linalg.inv(mat)
-print("Correction : \n",i)
+def P(A):
+	n=len(A)
+	I=np.eye(n)
+	Ak=A
+	P=[1]
+	for k in range(1,n+1):
+		c=-np.trace(Ak)/k
+		P.append(c)
+		Ak=np.dot(A,Ak+c*I)
+	return P
 
-j = methodeTraces(mat)
-print("Résultat : \n",j)
+def matToString(mat):
+	a = mat[2:-2]
+	text = ""
+	hello = 0
+	for i in range(len(a)):
+		if (a[i] == ']'):
+			hello = 1
+		elif (a[i] == '[') & hello == 1:
+			hello = 0
+			text += ';'
+		elif hello == 0:
+			text += a[i]
+	return text
 
+for i in range(len(sys.argv)):
+	print("argument n°",i," : ",sys.argv[i])
 
-if i.any() == j.all():
-	print("OK")
+"""
+if len(sys.argv) == 3:
+
+	mode = sys.argv[1]
+	argMat = sys.argv[2]
+	mat = np.matrix(argMat)
+
+	
+	matInv,det = matInverse(mat)
+	pol = str(P(mat))
+
+	if mode == 1:
+		print(matToString(matInv))
+	elif mode == 2:
+		print(det)
+	elif mode == 3:
+		a = pol[1:-1]
+		text = ""
+		for i in range(len(a)):
+			if a[i] == ',':
+				text += ";"
+			else:
+				text += a[i]
+		print(text)
+	else:
+		print("e1")
+else:
+	print("e2")
+"""
