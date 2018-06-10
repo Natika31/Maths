05a48191d64b1c91bc6900ed11da1db9a8bc904d
Unknown <natika.orsas@gmail.com>
2018-06-10 02:01:44 +0200

    corrige

--- vue/inputMatrice.php
+++ vue/inputMatrice.php
@@ -34,22 +34,25 @@
       </span>
 </form>
 <br>
-<h3> Entrez vos valeurs : </h3>
+<?php $dimension = $_POST['dimension'] ?? 3;?>
 
+<h3> Entrez vos valeurs pour cette matrice <?php echo $dimension.'x'.$dimension; ?></h3>
   <table id='matrice'>
-    <form
-    <tbody>
-      <?php $dimension = $_POST['dimension'] ?? 3;
-
-      for ($l = 0; $l < $dimension; $l++) : ?>
-      <tr class= "ligne">
-        <?php for ($c = 0; $c < $dimension; $c++) : ?>
-        <td >
-            <input id="x" name="'a'.$l.$c" type="text" class="form" type="text">
-         </td>
-        <?php endfor; ?>
-      </tr>
-    <?php endfor; ?>
-  </tbody>
+    <?php $matrice = array(); ?>
+    <form class="matrice" method="post" action="index.php">
+      <tbody>
+        <?php for ($l = 0; $l < $dimension; $l++) :  ?>
+        <tr class= "ligne">
+          <?php for ($c = 0; $c < $dimension; $c++) : ?>
+            <td >
+              <input id="x" name= 'cellule' type="text" class="form" >
+              <?php/* $matrice[$l][$c] */$a = $_POST('cellule'); echo $a; */?>
+            </td>
+          <?php endfor; ?>
+        </tr>
+      <?php endfor;?>
+    </tbody>
+    <button class="matrice" type="submit"  id= "btCalcul">Calculer</button>
+  </form>
 </table>
 </fieldset>
